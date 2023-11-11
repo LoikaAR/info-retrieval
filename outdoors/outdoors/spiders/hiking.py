dd0616ca13cf3af2a288c95324bfb27357fb4dc9
loikaa <arseni.loika@usi.ch>
2023-11-11 13:26:55 +0100

    initial working version of crawler for ticino-top-ten

--- outdoors/outdoors/spiders/hiking.py
+++ outdoors/outdoors/spiders/hiking.py
@@ -4,18 +4,20 @@ class QuoteSpider(scrapy.Spider):
 
     name = "hikes"    # Your spider name. Each instance of a QuoteSpider will share the same name.
 
-    start_urls = ["https://www.myswitzerland.com/en-ch/scoprire-la-svizzera/estate-autunno/listicles/wandern-2020/"] # URLs list to start crawling.
+    start_urls = ["https://www.ticinotopten.ch/en/trekking"] # URLs list to start crawling.
 
     def parse(self, response): # Funcion called every crawled web page. The response parameter will contain the web site response.
-        for element in response.xpath("//div[@class='site-content']"): 
-            
-            hike_name = element.xpath(".//div[@class='l-Gap m-down']/text()").get()    
-            # author = hike.xpath(".//small[@class='author']/text()").get()	           
+        for element in response.xpath("//div[@class=' item-list col-md-6 col-sm-12 ']"):
+
+            print(element)
+
+            hike_name = element.xpath(".//div[@class='item-text']/div[@class='title']/text()").get()    
+            category = element.xpath(".//div[@class='category']/text()").get()	           
             # tags = hike.xpath(".//div[@class='tags']/a[@class='tag']/text()").getall()
 
-            yield {'hike_name': hike_name}
+            yield {'hike_name': hike_name, 'category': category}
 
-        next_page = response.xpath("//li[@class='next']/a/@href").get()    # Extract next page link as a string.
+        next_page = response.xpath("//li[@class='nav navbar-primary']/a/@href").get()    # Extract next page link as a string.
 
         if next_page:	 # If next page is not None, that is, if we have a next page to visit...
-            yield response.follow(next_page, callback=self.parse)    # ...follow the next page link. Return the next page response object.
+            yield response.follow(next_page, callback=self.parse)    # ...follow the next page link. Return the next page response object
