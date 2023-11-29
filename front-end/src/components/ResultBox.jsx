b7190ef3f4822f40fde60d15e324705a519c3831
Delara Lomen <lomend@usi.ch>
2023-11-29 15:09:42 +0100

    finalizing the result box

--- front-end/src/components/ResultBox.jsx
+++ front-end/src/components/ResultBox.jsx
@@ -1,19 +1,35 @@
+{/* res.json object fields example:
+{"name": "360° Loop Gornergrat (Nr. 16)", "region": "Valais", "category": "Hiking trail", "distance": "0,5 km", "duration": "0,20 h", "ascent": "19 m"},
+         */}
+
 const ResultBox = () => {
-    return (
-      <div className="result-box">
-        <h2 className="result-title">
-            Search Result
-            
-        </h2>
-        <h3 className="result-category">
-            Category: aaaaa
+  return (
+    <div className="result-box">
+      {/* <a className="result-link" href="https://www.geeksforgeeks.org/" target="_blank"  rel="noreferrer"><h2 className="result-title">
+        Search Result (name)
+      </h2></a> */}
+      <a className="result-link" href="https://www.google.com/" target="_blank"  rel="noreferrer"><h2 className="result-title">
+        Search Result (name)
+      </h2></a>
+      <div className="result-details">
+        <h3 className="result-stats">
+          Distance: aaa
+          <br />
+          Duration: aaa
+          <br />
+          Ascent: aaa
         </h3>
-        <p className="result-description">
-            {/* TO DO: handle too lengthy text and print only sentences that fall within the word limit*/}
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam nulla porttitor massa id. Nam libero justo laoreet sit amet cursus sit amet dictum. Mi sit amet mauris commodo quis imperdiet massa tincidunt nunc. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Lacus laoreet non curabitur gravida. Varius morbi enim nunc faucibus a.
-        </p>
+        <h3 className="result-category">
+          Category: aaaaa
+          <br />
+          Region: aaa
+        </h3>
       </div>
-    );
-  };
-  
-  export default ResultBox;
+      <p className="result-description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam nulla porttitor massa id. Nam libero justo laoreet sit amet cursus sit amet dictum. Mi sit amet mauris commodo quis imperdiet massa tincidunt nunc. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Lacus laoreet non curabitur gravida. Varius morbi enim nunc faucibus a.
+      </p>
+    </div>
+  );
+};
+
+export default ResultBox;
