7f7ec70c03b3913e73a8fc1f77d470514bf83c3c
loikaa <arseni.loika@usi.ch>
2023-11-29 16:47:40 +0100

    added description and links to every item

--- outdoors/outdoors/spiders/activities_ti.py
+++ outdoors/outdoors/spiders/activities_ti.py
@@ -17,8 +17,38 @@ class OutdoorSpider(scrapy.Spider):
 
     def parse(self, response): 
         for element in response.xpath("//div[@class=' item-list col-md-6 col-sm-12 ']"):
-            print(element)
             name = element.xpath(".//div[@class='item-text']/div[@class='title']/text()").get()
             category = element.xpath(".//div[@class='category']/text()").get()
 
-            yield {'name': name, 'region': 'Ticino', 'category': category, 'distance': 'n/a km', 'duration': 'n/a h', 'ascent': 'n/a m'}
+            link = element.xpath('a/@href').get()
+            link = 'https://www.ticinotopten.ch' + str(link)
+
+            yield scrapy.Request(
+                url=response.urljoin(link),
+                callback=self.parse_inner,
+                cb_kwargs={
+                            'name': name, 
+                            'region': 'Ticino', 
+                            'category': category, 
+                            'distance': 'n/a km', 
+                            'duration': 'n/a h', 
+                            'ascent': 'n/a m', 
+                            'link': link
+                        }
+            )
+
+    def parse_inner(self, response, name, region, category, distance, duration, ascent, link):
+        desc_short = response.xpath("//div[@class='border-right-lg']/p[@id='short-text']/em/text()").get()
+        desc_main = response.xpath("//div[@id='text']/p/text()").getall()
+        description = [desc_short] + desc_main
+
+        yield {
+                'name': name, 
+                'region': region, 
+                'category': category, 
+                'distance': distance, 
+                'duration': duration, 
+                'ascent': ascent, 
+                'link': link,
+                'description': description
+            }
