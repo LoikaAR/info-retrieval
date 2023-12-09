3fdd90d4e644a4e5a08fe47a93a62bc55a2a7af7
Delara Lomen <lomend@usi.ch>
2023-12-09 14:58:08 +0100

    perfecting query highlighting

--- front-end/src/components/HighlightedText.jsx
+++ front-end/src/components/HighlightedText.jsx
@@ -2,13 +2,21 @@ import PropTypes from 'prop-types';
 import { removeStopwords } from './RemoveStopwords';
 
 const HighlightedText = ({ text, query }) => {
-  const queryWords = removeStopwords(query.query.toLowerCase()).split(/\s+/).filter(Boolean);
+  function removePunctuation(word) {
+    return word.replace(/[^\w]/g, '');
+  }
+  var queryWords = removeStopwords(query.query.toLowerCase()).split(/\s+/).filter(Boolean);
+  
+  const preprocessedWords = queryWords.map(function(word) {
+    return removePunctuation(word)
+  });
+  
   const words = text.split(/\b/);
 
   return (
     <div className="result-description">
       {words.map((word, index) => {
-        const isHighlighted = queryWords.includes(word.trim().toLowerCase()) && word.trim().length > 0;
+        const isHighlighted = preprocessedWords.includes(word.trim().toLowerCase()) && word.trim().length > 0;
         return (
           <span key={index} style={{ backgroundColor: isHighlighted ? '#93cf72' : 'transparent', borderRadius:  '5px'}}>
             {word}
